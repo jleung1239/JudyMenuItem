1c1df79160bc9a7b0059a9b7cbc4a708a2258949
BT_1N3_13 <a>
2018-11-27 08:41:58 -0500

    a

--- src/com/company/Trio.java
+++ src/com/company/Trio.java
@@ -5,17 +5,17 @@ public class Trio implements MenuItem{
     Salad salad;
     Drink drink;
 
-    public Trio(Sandwich, Salad, Drink){
+    public Trio(Sandwich sandwich, Salad salad, Drink drink){
         this.sandwich = sandwich;
         this.salad = salad;
         this.drink = drink;
     }
 
     public String getName(){
-        return sandwich + "/" + salad + "/" + drink + " Trio";
+        return sandwich.getName() + "/" + salad.getName() + "/" + drink.getName() + " Trio";
     }
 
-    public double getPrice(){
+    public double getPrice() {
         double price1 = sandwich.getPrice();
         double price2 = salad.getPrice();
         double price3 = drink.getPrice();
@@ -23,17 +23,26 @@ public class Trio implements MenuItem{
         double a;
         double b;
 
-        if (price1 > price2){
-            if (price1 > price3){
+        if (price1 > price2) {
+            if (price1 > price3) {
                 a = price1;
-                if (price2 > price3){
+                if (price2 > price3) {
                     b = price2;
-                }
-                else
-                {
+                } else {
                     b = price3;
                 }
-            
+
+            } else if (price3 > price1) {
+                a = price2;
+                b = price3;
+
+            }
+
+
         }
+        else if ((price2 > price1) && (price2 > price3)){
+            if (price2 > price3)
+        }
+        return a + b;
     }
 }
